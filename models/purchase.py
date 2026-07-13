a62ceeedcbb53d380225e44262697df4f3c80e02
Jimmie Faulkner <jfaulkner@catalyte.io>
2026-07-13 11:53:25 -0500

    Added purchase class code to purchase model

---
+++ models/purchase.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from datetime import datetime, timezone
+from decimal import Decimal
+from models.customer import Customer
+
+@dataclass
+class Purchase:
+    """
+        Represents a purchase with cost and measurement information.
+    """
+    timestamp: datetime
+    items: list[any]
+    total_cost: Decimal
+    customer: Customer
+
+    def __post_init__(self):
+        self.timestamp = datetime.now(timezone.utc)
